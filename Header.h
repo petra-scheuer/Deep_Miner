4114f6a3d55a78b7de1987c510c9b5fcec5afeb4
petra-scheuer <petra.scheuer.p@gmail.com>
2024-03-27 09:46:45 +0100

    Initial Commit

---
+++ Header.h
@@ -0,0 +1,9 @@
+//
+// Created by Petra Scheuer on 27.03.24.
+//
+
+#ifndef DEEP_MINER_HEADER_H
+#define DEEP_MINER_HEADER_H
+#include <iostream>
+
+#endif //DEEP_MINER_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Header.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
